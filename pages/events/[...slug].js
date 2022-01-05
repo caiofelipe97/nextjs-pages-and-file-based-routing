76cd00ebbbbe11ac53d09b277d41ee71175557ec
caiofam <caio.melo@trio.dev>
2022-01-04 22:39:00 -0300

    Extracting Data on the Catch-All Page

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,4 +1,34 @@
+import { useRouter } from "next/router";
+import { getFilteredEvents } from "../../dummy-data";
+
 function FilteredEventsPage() {
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid Filter. Please adjust your values!</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth });
+
   return (
     <div>
       <h1>Filtered Events Page</h1>
